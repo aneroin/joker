9520e35f76a2f2ad6576e4c40c882a21c7e0c8af
aneroin <halileo@ukr.net>
2015-09-21 17:25:17 +0300

    commented
    added some comments

--- app/models/index_model.php
+++ app/models/index_model.php
@@ -3,13 +3,16 @@ session_start();
   class Index_Model extends Model {
    public function __construct($lang = 'ua', $city = 'te') {
     parent::__construct();
-
+    //pre init
     $this->data['title'] = 'DataBase is OFFLINE';
-
+    //connecting to db
     require ('dbcon.php');
+    //statement preparing
     $sql = "SELECT * FROM Blocks WHERE Blocks.local=? AND Blocks.name = 'title' AND Blocks.idPages=1";
     $stm = $pdo->prepare($sql);
+    //statement executing
     $stm->execute(array($city));
+    //fetching array
     while ($db_data = $stm->fetch(PDO::FETCH_ASSOC)){
       $this->data['title'] = $db_data[$lang];    
     }
