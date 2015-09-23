d422791618e8e38be67fbf0214e47d30863b6cc7
aneroin <halileo@ukr.net>
2015-09-23 15:06:32 +0300

    grid, navigation and styles
    added css distribution
    added sidebar
    added responsive grid
    rewrited basic grid

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,8 +1,16 @@
 <?php require 'views/header.php'; ?>
-<hr>
+<a href="#menu-toggle" class="btn btn-yellow" id="menu-toggle-wrapper">
+	<img border="0" alt="toggle menu" src="<?php echo URL; ?>img/sidebar.gif" width="32" height="32">
+</a>
 <div>
 	<h3>
 		<?php echo $data['title'] ?>
 	</h3>
 </div>
+<script>
+    $("#menu-toggle-wrapper").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
 <?php require 'views/footer.php'; ?>
