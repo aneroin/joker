2bbcf8c03e65e2484f1ff2ffc4445c50d5f7713a
aneroin <halileo@ukr.net>
2015-10-01 11:53:06 +0300

    modal btn
    modal btn dynamic caption

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -105,8 +105,8 @@
 		<!-- Modal content-->
 		<div class="modal-content">
 		  <div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal">callback</button>
-			<h4 class="modal-title">передзвоніть мені</h4>
+			<button type="button" class="close" data-dismiss="modal"><?php echo $data['close'] ?></button>
+			<h4 class="modal-title black"><?php echo $data['second-btn'] ?></h4>
 		  </div>
 		  <div class="modal-body">
 		 <form role="form">
