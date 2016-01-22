d4b0ac3b4d6d6c173ca0a51a7243d33c9d7c63d9
aneroin <halileo@ukr.net>
2016-01-22 16:17:28 +0200

    geotagging
    added html5 geotagging
    added google reverse geocoding

--- app/controllers/m.php
+++ app/controllers/m.php
@@ -5,48 +5,44 @@ session_start();
 			parent::__construct();
 		}
 
-		public function Index($locale = null) {
-			require_once '../protected/ipvars.php';
-			require_once 'ipInfo.inc.php';
-			
-			$ipInfo = new ipInfo ($ip_key, 'json');
-			
-					
+		public function Index($locale = null) {		
 			$title = "Taxi Joker";
 			//if locale param is set - setting up session variables
 			if (isset($locale)) {
 				$_SESSION['lang'] = $locale['0'];
 				$_SESSION['local'] = $locale['1'];
 			} else {
-				if (!isset($_SESSION['lang'])) {
-					$userIP = $ipInfo->getIPAddress();
-					$location = json_decode($ipInfo->getCity($userIP),true);
-					if (preg_match($regions['ua'],$location['countryCode'])) {
-						$_SESSION['lang'] = 'ua';
-					} else if (preg_match($regions['ru'],$location['countryCode'])) {
-						$_SESSION['lang'] = 'ru';
-					} else {
-						$_SESSION['lang'] = 'eng';
-					}
-				}
-				if (!isset($_SESSION['local'])) {
-					$userIP = $ipInfo->getIPAddress();
-					$location = json_decode($ipInfo->getCity($userIP),true);
-					if (preg_match($regions['te'],$location['regionName'])) {
-						$_SESSION['local'] = 'te';
-					} else if (preg_match($regions['lu'],$location['regionName'])) {
-						$_SESSION['local'] = 'lu';
-					} else {
-						$_SESSION['local'] = 'te';
-					}
-				}
+				if (!isset($_SESSION['lang']))
+					$_SESSION['lang'] = 'ua';
+				if (!isset($_SESSION['local']))
+					$_SESSION['local'] = 'te';
 			}
 			//connecting to the model
 			require 'models/index_model.php';
 			//model init with locale params from sessing variables
     		$model = new Index_Model($_SESSION['lang'],$_SESSION['local']);
-    		//rendering index page
+    		//displaying index page
 			$this->view->display('material/index/index',$model->data,$title,true,false);
+		}
+
+		public function Contacts($locale = null) {
+			$title = "Taxi Joker - Contacts";
+			//connecting to the model
+			require 'models/index_model.php';
+			//model init with locale params from sessing variables
+    		$model = new Index_Model($_SESSION['lang'],$_SESSION['local']);
+    		//displaying index page
+			$this->view->display('material/contacts/index',$model->data,$title);
+		}
+
+		public function Prices($locale = null) {
+			$title = "Taxi Joker - Prices";
+			//connecting to the model
+			require 'models/index_model.php';
+			//model init with locale params from sessing variables
+    		$model = new Index_Model($_SESSION['lang'],$_SESSION['local']);
+    		//displaying index page
+			$this->view->display('material/prices/index',$model->data,$title);
 		}
 	}
 ?>
--- app/language.php
+++ app/language.php
@@ -1,13 +1,67 @@
 <?php
+require_once '../protected/gvars.php';
 session_start();
 	//getting params
 	$lang = $_GET['lang'];
 	$local = $_GET['local'];
+	$ref = $_GET['ref'];
+	$lat = $_GET['lat'];
+	$lon = $_GET['lon'];
 	//rewriting session vars
 	if (isset($lang))
 	$_SESSION['lang'] = $lang;
 	if (isset($local))
 	$_SESSION['local'] = $local;
 	//returning
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
+	if (!(isset($lang) || isset($local))) {
+		if (isset($lat) && isset($lon)) {
+			//Now build the actual lookup
+	        $address_url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' . $lat . ',' . $lon . '&sensor=false';
+	        $address_json = json_decode(file_get_contents($address_url));
+
+	        $address_data = $address_json->results[0]->address_components;
+
+	        $distr = $address_data[3]->long_name;    
+	        $country = $address_data[4]->short_name;
+
+	        if (preg_match($regions['ua'],$country)) {
+	        	$_SESSION['lang']="ua";
+	        	$geoValue = "true";
+	        } else if (preg_match($regions['ru'],$country)) {
+	        	$_SESSION['lang']="ru";
+	        	$geoValue = "true";
+	        } else {
+	        	$_SESSION['lang']="eng";
+	        	$geoValue = "false";
+	        }
+
+	        if (preg_match($regions['te'],$distr)) {
+	        	$_SESSION['local'] ="te";
+	        	$geoValue = "true";
+	        } else if (preg_match($regions['lu'],$distr)) {
+				$_SESSION['local'] ="lu";
+				$geoValue = "true";
+	        } else {
+	        	$_SESSION['local'] ="te";
+	        	$geoValue = "false";
+	        }
+	        if ($geoValue=="false") 
+	        	setcookie("GeoLocated", $geoValue, time()-3600);
+	        setcookie("GeoLocated", $geoValue, time()+3600*7);
+	    }
+	}
+	if (isset($ref)) {
+		if ($ref=="no") {
+			echo "town ".$town." county ".$county."<br>";
+			echo "<pre>";
+			print_r($address_data);
+			echo "</pre>";
+		}
+		else {
+			header('Location: ' . 'http://taxijoker.com/'.$ref);
+		}
+		
+	} else {
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
+	}
 ?>
