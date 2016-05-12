1ee0818d2a67228ee60b8a35f4d737055d36d824
aneroin <halileo@ukr.net>
2016-05-12 18:40:30 +0300

    Router
    routing strategy incapsulated in class and documented

--- app/libs/Router.php
+++ app/libs/Router.php
@@ -1,23 +1,92 @@
 <?php
+ /**
+  * Router Class, created by INshadow
+  */
   class Router {
 	$pregs = array();
+	$ends = array();
 	$local = '/^(.{2}-.{2})$/';
 
     public function __construct() {
    	
     }
-
-    public function add($name, $preg) {
+  	/**
+  	* Adds a new route to router
+  	*
+  	* Can be used to add named routes and check input urls for revelant route
+	* or to associate name with method and call them by name
+  	*
+  	* @param string $name - name of the route
+	*
+	* @param string $preg - regular pattern to check url with
+	*
+	* @param array $end <array('ClassName', 'MethodName')> - endpoint to call by name
+  	*
+  	* @return void
+  	*/
+    public function add($name, $preg, $end) {
 		$val = '/'+$preg+'/';
    		$pregs += [$name => $val];
+		$ends += [$name => $end];
     }
-	  
+	
+	/**
+  	* Checks a route in router
+  	*
+  	* Can be used to check a url pattern is vaid for named route
+  	*
+  	* @param string $name - name of the route
+	*
+	* @param string $val - url to check
+	*
+  	* @return boolean
+  	*/  
 	public function check($name, $val){
 		if (preg_match($pregs[$name],$val)==1) 
 		return true;
 		else return false;
 	}
+	
+	/**
+  	* Road a route in router
+  	*
+  	* Can be used to get route in router
+  	*
+  	* @param string $name - name of the route
+	*
+	* @param string $val - url to check
+	*
+	* @param array $params - parameters to pass into endpoint
+	*
+  	* @return void
+  	*/  
+	public function road($name, $val=NULL, $params=NULL){
+		if ($val!=NULL){
+			if (check($name, $val)){
+				if ($params!=NULL) {
+					$ends[$name]($params);
+				} else {
+					$ends[$name]();
+				}
+			}
+		} else {
+			if ($params!=NULL) {
+					$ends[$name]($params);
+				} else {
+					$ends[$name]();
+				}
+		}
+	}
 
+	/**
+  	* Test a url in Router and call endpoint
+  	*
+  	* Can be used to work in auto mode and call endpoints like <host>/controller/method
+  	*
+  	* @param string $url - url to test
+	*
+  	* @return void
+  	*/
     public function test($url){
 		$url = rtrim($url, '/');
 	    $url = ltrim($url, '/');
